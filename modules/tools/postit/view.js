66f8fe69b9ea66d09108f46b604053f2b246e60d
5willrod <5willrod@informatik.uni-hamburg.de>
2018-11-05 14:12:39 +0100

    draft UI

--- modules/tools/postit/view.js
+++ modules/tools/postit/view.js
@@ -5,21 +5,52 @@ const View = Backbone.View.extend({
     template: _.template(PostitTemplate),
     // wird aufgerufen wenn die View erstellt wird
     initialize: function () {
-        //this.render();
         this.listenTo(this.model, {
             "change:isActive change:url": this.render,
+            "click button": this.testcall,
         });
+        this.testcall();
         // Bestätige, dass das Modul geladen wurde
         Radio.trigger("Autostart", "initializedModul", this.model.get("id"));
     },
     id: "postit",
     model: new PostitModel(),
     render: function (model, value) {
-        console.log("rendered");
-        $(this.el).html(this.template());
-        $("body").append(this.$el);
+        if (value) {
+            this.setElement(document.getElementsByClassName("win-body")[0]);
+            this.model.createInteraction();
+            this.$el.html(this.template(model.toJSON()));
+            this.changedPosition();
+            this.delegateEvents();
+        }
+        else {
+            this.model.setUpdatePosition(true);
+            this.model.removeInteraction();
+            this.undelegateEvents();
+        }
         return this;
-      }
+      },
+      testcall: function(){
+        console.log("testcall called");
+        /*
+        $.get("http://localhost:8080/get/bier", function(data){
+            console.log("success");
+        }).fail(function(error){
+            console.log("error");
+            console.log(error);
+        })
+        var request = $.ajax("http://localhost:8080/get/bier")
+            .done(function() {
+                console.log("success");
+            })
+            .fail(function(jq,textStatus,httperror) {
+                console.log(jq);
+                console.log(textStatus);
+                console.log(httperror);
+            })
+            .always(function() {
+            });  */ 
+      },
   });
 
 export default View;
