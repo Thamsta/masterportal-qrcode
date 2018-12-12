84863c62bb84a10b0e6305d010dc36a31d41cc95
5cmuelle <christoph.mueller-3@studium.uni-hamburg.de>
2018-12-12 18:04:45 +0100

    cm_2018.12.12

--- modules/tools/postit_qr/model.js
+++ modules/tools/postit_qr/model.js
@@ -6,28 +6,26 @@ import VectorLayer from "ol/layer/Vector";
 import VectorSource from 'ol/source/Vector.js';
 import Point from "ol/geom/Point.js";
 import Feature from "ol/Feature.js";
-import { Circle as CircleStyle, Style, Fill, Stroke } from "ol/style.js";
+import { Circle as CircleStyle, Style, Fill } from "ol/style.js";
+import Select from 'ol/interaction/Select.js';
+import click from 'ol/events/condition.js';
 
 const Model = Tool.extend({
     defaults: _.extend({}, Tool.prototype.defaults, {
         selectPointerMove: null,
+        selectInteraction: undefined,
         positionMapProjection: [],
         currentProjectionName: "EPSG:25832",
-
         qrBild: undefined,
-        qrOverlay: new Overlay({
-            position: undefined,
-            stopEvent: false,
-            element: undefined,
-            autoPan: true,
-            autoPanAnimation: {
-                duration: 250
-            },
-        }),
-        // qrArray: [],
-
-
+        qrOverlay: undefined,
+        postitLayer: undefined,
+        postitFeatures: undefined,
+        edit: true,
+        view: false,
+        selectOverlay: undefined,
+        selected_feature_content: undefined,
     }),
+
     initialize: function () {
         this.superInitialize();
         this.listenTo(this, {
@@ -35,17 +33,43 @@ const Model = Tool.extend({
                 Radio.trigger("MapMarker", "hideMarker");
             },
         });
+        this.setqrOverlay(
+            new Overlay({
+                position: undefined,
+                stopEvent: false,
+                element: undefined,
+                autoPan: true,
+                autoPanAnimation: {
+                    duration: 250
+                },
+            }));
+        this.setSelectOverlay(
+            new Overlay({
+                position: undefined,
+                stopEvent: false,
+                element: undefined,
+                autoPan: true,
+                autoPanAnimation: {
+                    duration: 250
+                },
+            })
+        );
+        this.setSelectInteraction(
+            new Select({
+                condition: click,
+            })
+        )
     },
 
     createQrLayer: function () {
+        var that = this;
         $.ajax({
             url: "https://thawing-brushlands-15739.herokuapp.com/postit/all.json",
-            //data: params,
             async: true,
             type: "GET",
             context: this,
             success: function (data) {
-                var features = [];
+                that.setPostitFeatures([]);
                 var pointstyle = new Style({
                     image: new CircleStyle({
                         radius: 7,
@@ -54,34 +78,64 @@ const Model = Tool.extend({
                         })
                     })
                 });
-
                 for (var i = 0; i < data.length; i++) {
                     var object = data[i];
-                    var objectGeometry = object.geometry.coordinates;
-                    var objectTitle = object.properties.titel;
-                    var objectInhalt = object.properties.inhalt;
-                    var objectTag = object.properties.tag;
                     var point = new Feature({
-                        geometry: new Point(objectGeometry),
-                        name: objectTitle,
-                        inhalt: objectInhalt,
-                        tag: objectTag
+                        geometry: new Point(object.coords),
+                        name: object.title,
+                        inhalt: object.content,
+                        tag: object.tags
                     });
                     point.setStyle(pointstyle);
-                    features.push(point);
+                    that.getPostitFeatures().push(point);
                 }
                 var source = new VectorSource({});
+                source.addFeatures(that.getPostitFeatures());
                 var layer = new VectorLayer({
                     source: source,
                 });
-
-                layer.setVisible(true);
-                Radio.trigger("Map", "addLayer", layer);
-                source.addFeatures(features);
+                this.setPostitLayer(layer);
+                layer.setVisible(false);
+                Radio.trigger("Map", "addLayer", this.getPostitLayer());
             },
         });
     },
+    removeQrLayer: function () {
+        this.getPostitLayer().setVisible(false);
+    },
 
+    toggleQrLayer: function () {
+        if (this.getPostitLayer().getVisible() === true) {
+            this.getPostitLayer().setVisible(false);
+            this.removeSelectInteraction();
+        }
+        else {
+            this.getPostitLayer().setVisible(true);
+            Radio.trigger("Map", "addInteraction", this.getSelectInteraction());
+        }
+    },
+    createSelectInteraction: function () {
+        var that = this;
+        that.getSelectInteraction().on("select", function () {
+            that.selectClicked();
+        });
+        Radio.trigger("Map", "addInteraction", this.getSelectInteraction());
+    },
+    removeSelectInteraction: function () {
+
+        Radio.trigger("Map", "removeInteraction", this.getSelectInteraction());
+    },
+    selectClicked: function () {
+        if (this.getSelectInteraction().getFeatures().item(0)) {
+            var selected_feature = this.getSelectInteraction().getFeatures().item(0),
+                title = selected_feature.get("name"),
+                tag = selected_feature.get("tag"),
+                inhalt = selected_feature.get("inhalt"),
+                coord = selected_feature.get("geometry");
+            this.set("selected_feature_content", [title, tag, inhalt, coord]);
+            Radio.trigger("Map", "addOverlay", this.getSelectOverlay());
+        }
+    },
     //Creates Mouse interaction and binds to function
     createInteraction: function () {
         this.setSelectPointerMove(new Pointer({
@@ -89,13 +143,12 @@ const Model = Tool.extend({
                 this.positionClicked(evt.coordinate);
             }.bind(this)
         }, this));
-
-        Radio.trigger("Map", "addInteraction", this.get("selectPointerMove"));
+        Radio.trigger("Map", "addInteraction", this.getSelectPointerMove());
     },
 
     //Removes all mouse interaction
     removeInteraction: function () {
-        Radio.trigger("Map", "removeInteraction", this.get("selectPointerMove"));
+        Radio.trigger("Map", "removeInteraction", this.getSelectPointerMove());
         this.set("selectPointerMove", null);
     },
 
@@ -120,13 +173,13 @@ const Model = Tool.extend({
             if (err) throw err
             that.setqrBild(url);
         });
-        Radio.trigger("Map", "addOverlay", this.get("qrOverlay"));
+        Radio.trigger("Map", "addOverlay", this.getqrOverlay());
         this.setPositionMapProjection(position);
     },
 
     //Transforms and returns the current position to a certain projection (atm always called with EPSG:25832 )
     returnTransformedPosition: function (targetProjection) {
-        var positionMapProjection = this.get("positionMapProjection"),
+        var positionMapProjection = this.getPositionMapProjection(),
             positionTargetProjection = [0, 0];
 
         if (positionMapProjection.length > 0) {
@@ -161,9 +214,69 @@ const Model = Tool.extend({
         this.set("qrOverlay", value);
     },
 
+    // getter for qrOverlay
+    getqrOverlay: function () {
+        return this.get("qrOverlay");
+    },
+
+    // setter for postitLayer
+    setPostitLayer: function (value) {
+        this.set("postitLayer", value);
+    },
+
+    // getter for postitLayer
+    getPostitLayer: function () {
+        return this.get("postitLayer");
+    },
+
+    // setter for postitFeatures
+    setPostitFeatures: function (value) {
+        this.set("postitFeatures", value);
+    },
+
+    // getter for postitFeatures
+    getPostitFeatures: function () {
+        return this.get("postitFeatures");
+    },
+
+    // setter for selectOverlay
+    setSelectOverlay: function (value) {
+        this.set("selectOverlay", value);
+    },
+
+    // getter for selectOverlay
+    getSelectOverlay: function () {
+        return this.get("selectOverlay");
+    },
+
+    // setter for selected_feature_content
+    setSelectFeatureContent: function (value) {
+        this.set("selected_feature_content", value);
+    },
+
+    // getter for selected_feature_content
+    getSelectFeatureContent: function () {
+        return this.get("selected_feature_content");
+    },
+
     // setter for selectPointerMove
     setSelectPointerMove: function (value) {
         this.set("selectPointerMove", value);
+    },
+
+    // getter for selectPointerMove
+    getSelectPointerMove: function () {
+        return this.get("selectPointerMove");
+    },
+
+    // setter for setSelectInteraction
+    setSelectInteraction: function (value) {
+        this.set("selectInteraction", value);
+    },
+
+    // getter for setSelectInteraction
+    getSelectInteraction: function () {
+        return this.get("selectInteraction");
     },
 
     // setter for positionMapProjection
--- modules/tools/postit_qr/view.js
+++ modules/tools/postit_qr/view.js
@@ -3,71 +3,115 @@ import PostItQRModel from "./model";
 
 const PostItQRView = Backbone.View.extend({
     template: _.template(PostItQRTemplate),
+
     id: "postItQR",
     model: new PostItQRModel(),
     events: {
-        //input events
-        "click #deletePostitsButton": "hideQrMarker",
-        // "click #newPostitButton": "newPostit"
+        "click #toggleCreate": "toggleCreatePostItQR",
+        "click #toggleView": "togglePostItSelectView",
+        "click #select_popup_closer": "hidePostItSelect",
+        "click #qr_popup_closer": "hidePostItQR",
     },
-    
+
     initialize: function () {
         //listeners on model
         this.listenTo(this.model, {
             "change:isActive": this.render,
-            "change:positionMapProjection": this.createQR,
+            "change:positionMapProjection": this.createPostItQR,
+            "change:selected_feature_content": this.createPostItSelectView,
         });
         // Bestätige, dass das Modul geladen wurde
         Radio.trigger("Autostart", "initializedModul", this.model.get("id"));
-        this.model.get("qrOverlay").setElement(this.$el[0]);
     },
 
     render: function (model, value) {
-        if (value) {
+        if (this.model.get("isActive") === true) {
             this.setElement(document.getElementsByClassName("win-body")[0]);
             this.$el.html(this.template(model.toJSON()));
+            $('#qr_popup')[0].style.visibility = "hidden";
+            $('#select_popup')[0].style.visibility = "hidden";
+
             this.delegateEvents();
-            this.model.createInteraction();
             this.model.createQrLayer();
-            document.getElementById("popup-content").style.visibility = "hidden";
+            this.model.createInteraction();
+            if (this.model.get("view")){
+                this.model.createSelectInteraction();
+            }
         }
         else {
-            this.hideQrMarker();
-            this.model.get("qrOverlay").setElement(this.$el[0]);
-            $("#map").removeClass("cursor-crosshair");
-            $("#cursorGlyph").remove();
-            $("#map").off("mousemove");
+            this.hidePostItQR();
+            this.hidePostItSelect();
+            this.model.removeSelectInteraction();
             this.model.removeInteraction();
+            this.model.removeQrLayer();
             this.undelegateEvents();
         }
     },
 
-    createQR: function () {
+    createPostItSelectView: function () {
+        var selected_feature_content = this.model.getSelectFeatureContent();
+        var position2 = selected_feature_content[3].getCoordinates();
+        var content = $('#select_popup')[0];
+        $('#select_popup')[0].style.visibility = "visible";
+        $("#titel_content")[0].innerHTML = selected_feature_content[0];
+        $('#kategorie_content')[0].innerHTML = selected_feature_content[1];
+        $('#inhalt_content')[0].innerHTML = selected_feature_content[2];
+        this.model.getSelectOverlay().set("element", content);
+        this.showPostItSelect(position2);
+    },
+
+    createPostItQR: function () {
         var position = this.model.returnTransformedPosition(this.model.getCurrentProjectionName());
-        var content = document.getElementById('popup-content');
-        content.style.visibility = "visible";
+        var content = $('#qr_popup')[0];
+        $('#QRplaceHolderLabel')[0].innerHTML = "Scannen Sie den QR-Code mit Ihrem Endgerät.";
+        $('#image')[0].src = this.model.getqrBild();
+        this.model.getqrOverlay().set("element", content);
 
-        document.getElementById("QRplaceHolderLabel").innerHTML = "Scannen Sie den QR-Code mit Ihrem Endgerät.";
-        document.getElementById('image').src = this.model.getqrBild();
-        this.model.get("qrOverlay").set("element", content);
-        this.showQrMarker(position);
+        this.showPostItQR(position);
+    },
+
+    showPostItQR: function (coordinate) {
+        $('#qr_popup')[0].style.visibility = "visible";
+        this.model.getqrOverlay().setPosition(coordinate);
+    },
+
+    hidePostItQR: function () {
+        // $('#qr_popup')[0].style.visibility = "hidden";
+        this.model.getqrOverlay().setPosition(undefined);
 
     },
 
-    showQrMarker: function (coordinate) {
-        this.$el.show();
-        this.model.get("qrOverlay").setPosition(coordinate);
+    showPostItSelect: function (coordinate) {
+        this.model.getSelectOverlay().setPosition(coordinate);
     },
 
-    hideQrMarker: function () {
-        // this.$el.hide();
-        document.getElementById("popup-content").style.visibility = "hidden";
-
-        this.model.get("qrOverlay").setPosition(undefined);
+    hidePostItSelect: function () {
+        // $('#select_popup')[0].style.visibility = "hidden";
+        this.model.getSelectOverlay().setPosition(undefined);
     },
 
-    deletePostits: function () {
-        this.model.set("isActive", false);
+    togglePostItSelectView: function () {
+        if (this.model.get("view")) {
+            this.model.removeSelectInteraction();
+            this.hidePostItSelect();
+            this.model.set("view", false);
+        }
+        else {
+            this.model.createSelectInteraction();
+            this.model.set("view", true);
+        }
+        this.model.toggleQrLayer();
+    },
+    toggleCreatePostItQR: function () {
+        if (this.model.get("edit")) {
+            this.model.removeInteraction();
+            this.hidePostItQR();
+            this.model.set("edit", false);
+        }
+        else {
+            this.model.createInteraction();
+            this.model.set("edit", true);
+        }
     }
 });
 
